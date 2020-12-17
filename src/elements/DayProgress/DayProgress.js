95a08ba2c496f00130dc3e4a57e6f346cca8a859
ninedev <master-vva@narod.ru>
2020-12-17 09:42:35 +0300

    day progress bugfix

--- src/elements/DayProgress/DayProgress.js
+++ src/elements/DayProgress/DayProgress.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {ThemeContext} from '../../themes';
 import './DayProgress.less';
 
 export default function DayProgress(props) {
     const {progress, items} = props;
-    const currentProgress = progress.get(new Date().toLocaleDateString());
+    const currentProgress = progress.get(new Date().toLocaleDateString()) || {};
 
     let current = 0;
     Object.keys(currentProgress).forEach((key) => {
@@ -14,9 +15,14 @@ export default function DayProgress(props) {
     let width = Math.round((current * 100) / total);
     width = width ? {width: `${width}%`} : {width: 0};
 
+    const {settings} = useContext(ThemeContext);
+    const textTheme = {
+        color: settings.color,
+    };
+
     return (
         <div className="flex-column">
-            <div className="dayProgress-title">Day progress</div>
+            <div className="dayProgress-title" style={textTheme}>Day progress</div>
             <div className="dayProgress-bar">
                 <div
                     className="dayProgress-bar__percents"
