7914c22f0617c503b5d52f77a46db7579d9f3eab
ninedev <master-vva@narod.ru>
2020-12-21 16:36:15 +0300

    add buttons to number component

--- src/elements/InputField/InputField.js
+++ src/elements/InputField/InputField.js
@@ -1,10 +1,10 @@
 import React, {useContext} from 'react';
 import {ThemeContext} from '~/themes';
+import './InputField.less';
 
 export default function InputField(props) {
     const {settings} = useContext(ThemeContext);
-
-    const {inputId, inputType, placeholder, label, className, value, onChange} = props;
+    const {inputId, inputType, label, className, onChange} = props;
 
     const textTheme = {
         color: settings.color,
@@ -16,6 +16,18 @@ export default function InputField(props) {
 
     const containerClass = `flex-column ${className}`;
 
+    const changeHandler = (value) => {
+        let output = value;
+        if (inputType === 'number') {
+            output = +output;
+
+            if (output <= 0) {
+                output = 1;
+            }
+        }
+        return onChange(output);
+    };
+
     return (
         <div className={containerClass}>
             <Label
@@ -23,21 +35,19 @@ export default function InputField(props) {
                 caption={label}
                 theme={textTheme}
             />
-            <input
-                id={inputId}
-                style={{...inputTheme, ...textTheme}}
-                type={inputType}
-                placeholder={placeholder}
-                value={value}
-                min={1}
-                onChange={(ev) => {
-                    let output = ev.target.value;
-                    if (inputType === 'number') {
-                        output = +output;
-                    }
-                    return onChange(output);
-                }}
-            />
+            {inputType === 'number'
+                ? <ButtonWrapper changeHandler={changeHandler} {...props}>
+                    <Input
+                        theme={{...inputTheme, ...textTheme}}
+                        changeHandler={changeHandler}
+                        {...props}
+                    />
+                </ButtonWrapper>
+                : <Input
+                    theme={{...inputTheme, ...textTheme}}
+                    changeHandler={changeHandler}
+                    {...props}
+                />}
         </div>
     );
 }
@@ -54,5 +64,34 @@ function Label({caption, forId, theme}) {
         >
             {caption}
         </label>
+    );
+}
+
+function Input({inputId, inputType, placeholder, value, changeHandler, theme}) {
+    return (
+        <input
+            id={inputId}
+            className="input"
+            style={theme}
+            type={inputType}
+            placeholder={placeholder}
+            value={value}
+            min={1}
+            onChange={(ev) => changeHandler(ev.target.value)}
+        />
+    );
+}
+
+function ButtonWrapper({value, changeHandler, children}) {
+    const changeValue = (isIncrease) => {
+        changeHandler(isIncrease ? ++value : --value);
+    }
+
+    return (
+        <div className="flex-row input-number__container">
+            <button className="button-accented" onClick={() => changeValue()}>-</button>
+            <div className="input-number">{children}</div>
+            <button className="button-accented" onClick={() => changeValue(true)}>+</button>
+        </div>
     );
 }
